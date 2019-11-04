f4b2937961550b704fdecac2e338ef1aaf29fa3f
Shishir Jakati <shishir.jakati98@gmail.com>
2019-11-04 18:03:45 -0500

    Stupid hotfix tensorflow is stupid

--- DRN_Keras/basic_block.py
+++ DRN_Keras/basic_block.py
@@ -1,47 +1,47 @@
-"""
-Author: Shishir Jakati
+# """
+# Author: Shishir Jakati
 
-Inspired by implementation here: https://github.com/dmolony3/DRN/blob/master/DRN.py
-"""
-import keras
-from keras.layers import Add, BatchNormalization, Conv2D, Activation, Input
+# Inspired by implementation here: https://github.com/dmolony3/DRN/blob/master/DRN.py
+# """
+# import keras
+# from keras.layers import Add, BatchNormalization, Conv2D, Activation, Input
 
-def basic_block(input_channels, output_channels, name, strides=1, dilation=1, kernel=3, residual=True):
-    """
-    Defines the basic block as used in the DRN. 
+# def basic_block(input_channels, output_channels, name, strides=1, dilation=1, kernel=3, residual=True):
+#     """
+#     Defines the basic block as used in the DRN. 
 
-    Inputs:
-        - input_channels: Channel size of input tensor
-        - output_channels: Channel size of output tensor
-        - name: Name for the block
-        - strides: Stride length of convolutional filter
-        - dilation: Dilation rate of convolutional filter
-        - kernel: Kernel size of convolutional filter
-        - residual: Flag indicating whether the residual connection is used or not
+#     Inputs:
+#         - input_channels: Channel size of input tensor
+#         - output_channels: Channel size of output tensor
+#         - name: Name for the block
+#         - strides: Stride length of convolutional filter
+#         - dilation: Dilation rate of convolutional filter
+#         - kernel: Kernel size of convolutional filter
+#         - residual: Flag indicating whether the residual connection is used or not
     
-    Outputs:
-        - model: BasicBlock Keras model
-    """
+#     Outputs:
+#         - model: BasicBlock Keras model
+#     """
 
-    input_tensor = Input(shape=(None, None, input_channels))
-    ## save input tensor for residual connection
-    shortcut = input_tensor
-    x = input_tensor
+#     input_tensor = Input(shape=(None, None, input_channels))
+#     ## save input tensor for residual connection
+#     shortcut = input_tensor
+#     x = input_tensor
 
-    ## first conv level
-    y = Conv2D(output_channels, (kernel, kernel), strides=(strides, strides), dilation_rate=(dilation, dilation), padding="same", name=name + "_conv1")(x)
-    y = BatchNormalization(name=name + "bn_1")(y)
-    y = Activation('relu', name=name + "relu_1")(y)
+#     ## first conv level
+#     y = Conv2D(output_channels, (kernel, kernel), strides=(strides, strides), dilation_rate=(dilation, dilation), padding="same", name=name + "_conv1")(x)
+#     y = BatchNormalization(name=name + "bn_1")(y)
+#     y = Activation('relu', name=name + "relu_1")(y)
 
-    ## second conv level
-    y = Conv2D(output_channels, (kernel, kernel), strides=(strides, strides), dilation_rate=(dilation, dilation), padding="same", name=name+ "conv_2")(y)
-    y = BatchNormalization(name=name + "bn_2")(y)
+#     ## second conv level
+#     y = Conv2D(output_channels, (kernel, kernel), strides=(strides, strides), dilation_rate=(dilation, dilation), padding="same", name=name+ "conv_2")(y)
+#     y = BatchNormalization(name=name + "bn_2")(y)
 
-    ## optional residual connection
-    if residual:
-        y = Add(name=name + "add")([shortcut, y])
+#     ## optional residual connection
+#     if residual:
+#         y = Add(name=name + "add")([shortcut, y])
     
-    y = Activation('relu', name=name + "relu_2")(y)
+#     y = Activation('relu', name=name + "relu_2")(y)
 
-    ## return block
-    return keras.models.Model(inputs=input_tensor, outputs=y)
+#     ## return block
+#     return keras.models.Model(inputs=input_tensor, outputs=y)
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import numpy as np
 import keras
 import keras.backend as K
 import tensorflow as tf
-from DRN_Keras.drn import build_DRN26, build_DRN42
+# from DRN_Keras.drn import build_DRN26, build_DRN42
 from keras.applications.resnet50 import ResNet50
 from keras import regularizers
 from keras.layers import (Activation, BatchNormalization, Conv2D, Dropout,
@@ -139,3 +139,112 @@ class EAST_DRN_model():
         self.target_score_map = target_score_map
         self.pred_score_map = pred_score_map
         self.pred_geo_map = pred_geo_map
+
+def basic_block(input_channels, output_channels, name, strides=1, dilation=1, kernel=3, residual=True):
+    """
+    Defines the basic block as used in the DRN. 
+
+    Inputs:
+        - input_channels: Channel size of input tensor
+        - output_channels: Channel size of output tensor
+        - name: Name for the block
+        - strides: Stride length of convolutional filter
+        - dilation: Dilation rate of convolutional filter
+        - kernel: Kernel size of convolutional filter
+        - residual: Flag indicating whether the residual connection is used or not
+    
+    Outputs:
+        - model: BasicBlock Keras model
+    """
+
+    input_tensor = Input(shape=(None, None, input_channels))
+    ## save input tensor for residual connection
+    shortcut = input_tensor
+    x = input_tensor
+
+    ## first conv level
+    y = Conv2D(output_channels, (kernel, kernel), strides=(strides, strides), dilation_rate=(dilation, dilation), padding="same", name=name + "_conv1")(x)
+    y = BatchNormalization(name=name + "bn_1")(y)
+    y = Activation('relu', name=name + "relu_1")(y)
+
+    ## second conv level
+    y = Conv2D(output_channels, (kernel, kernel), strides=(strides, strides), dilation_rate=(dilation, dilation), padding="same", name=name+ "conv_2")(y)
+    y = BatchNormalization(name=name + "bn_2")(y)
+
+    ## optional residual connection
+    if residual:
+        y = Add(name=name + "add")([shortcut, y])
+    
+    y = Activation('relu', name=name + "relu_2")(y)
+
+    ## return block
+    return keras.models.Model(inputs=input_tensor, outputs=y)
+
+
+
+
+def build_DRN42(input_tensor):
+    """
+    Creates the DRNC_42 Model.
+    Inputs:
+        None
+    Outputs:
+        - model: DRNC_42 Keras Model
+    """
+    if input_tensor is None:
+        input_tensor = Input(shape=(None, None, 3))
+
+    ## input block
+    y = Conv2D(16, (7,7))(input_tensor)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
+
+    ## layer 1
+    block = basic_block(16, 16, "layer1", residual=False)
+    y = block(y)
+
+    ## layer 2
+    block = basic_block(16, 32, "layer2", strides=2, residual=False)
+    y = block(y)
+
+    ## layer 3
+    block = basic_block(32, 64, "layer3_1", strides=2, residual=False)
+    y = block(y)
+    block = basic_block(64, 64, "layer3_2", strides=2)
+    y = block(y)
+    y = block(y)
+
+    ## layer 4
+    block = basic_block(64, 128, "layer4_1", strides=2, residual=False)
+    y = block(y)
+    block = basic_block(128, 128, "layer4_1", strides=2)
+    y = block(y)
+    y = block(y)
+    y = block(y)
+
+    ## layer 5
+    block = basic_block(128, 256, "layer5_1", dilation=2, residual=False)
+    y = block(y)
+    block = basic_block(256, 256, "layer5_2", dilation=2)
+    y = block(y)
+    y = block(y)
+    y = block(y)
+    y = block(y)
+    y = block(y)
+
+    ## layer 6
+    block = basic_block(256, 512, "layer6_1", dilation=4, residual=False)
+    y = block(y)
+    block = basic_block(512, 512, "layer6_2", dilation=4)
+    y = block(y)
+    y = block(y)
+
+    ## layer 7
+    block = basic_block(512, 512, "layer7", dilation=2, residual=False)
+    y = block(y)
+
+    ## layer 8
+    block = basic_block(512, 512, "layer8", residual=False)
+    y = block(y)
+
+    return keras.models.Model(inputs=input_tensor, outputs=y)
